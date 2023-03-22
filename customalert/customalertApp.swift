a8bdeb81596b6e4856e8d97a840a34d5e3d70d60
Jonn Alves <jonatas.alsantos@jsl.com.br>
2023-03-22 20:49:13 -0300

    Initial Commit

---
+++ customalert/customalertApp.swift
@@ -0,0 +1,17 @@
+//
+//  customalertApp.swift
+//  customalert
+//
+//  Created by Jonn Alves on 22/03/23.
+//
+
+import SwiftUI
+
+@main
+struct customalertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
